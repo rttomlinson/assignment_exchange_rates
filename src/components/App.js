342aaff90914733f0d02c2acc01a446110d8ab96
Renzo Tomlinson <rttomlinson@gmail.com>
2017-05-03 21:27:53 +0000

    Add historicCurrency

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,88 @@
-import React, { Component } from "react";
+import React, {
+    Component
+}
+from "react";
 import ExchangeRatesDisplay from "./ExchangeRatesDisplay";
 import ExchangeRatesControls from "./ExchangeRatesControls";
 
 import "../App.css";
 
 class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      currentBase: "GBP"
+        this.state = {
+            currentBase: "GBP"
+        };
+        this.rates = {};
+        this.codes = [];
+        this.historicalRates = {};
+
+        this._dataFetch = this._dataFetch.bind(this);
+    }
+
+    componentDidMount() {
+        const rates = this._dataFetch(this.state.currentBase)
+            .then(json => {
+                this.setState({
+                    currentBase: json.base
+                });
+            })
+            .catch();
+
+        const historicalRates = this._dataFetch(this.state.currentBase)
+            .then(json => {
+                this.setState({
+                    currentBase: json.base
+                });
+            })
+            .catch();
+
+
+        Promise.all([rates, historicalRates])
+        .then(([rates, historicalRates]) => {
+            
+            
+        })
+
+    }
+
+    _dataFetch(base) {
+        return fetch(`https://api.fixer.io/latest?base=${base}`)
+            .then(response => response.json())
+            .then(json => {
+                this.rates = json.rates;
+                this.codes = Object.keys(this.rates);
+                return json;
+            });
+    }
+    
+    _historicDataFetch(base, base2, date) {
+        return fetch(`https://api.fixer.io/${date}?symbols=${base},${base2}`)
+            .then(response => response.json())
+            .then(json => {
+                this.rates = json.rates;
+                this.codes = Object.keys(this.rates);
+                return json;
+            });
+    }
+    
+    
+
+    onSelectChange = e => {
+        const newBase = e.target.value;
+        this._dataFetch(newBase)
+            .then(json => {
+                this.setState({
+                    currentBase: newBase
+                });
+            })
+            .catch();
     };
-    this.rates = {};
-    this.codes = [];
 
-    this._dataFetch = this._dataFetch.bind(this);
-  }
-
-  componentDidMount() {
-    this._dataFetch(this.state.currentBase)
-      .then(json => {
-        this.setState({
-          currentBase: json.base
-        });
-      })
-      .catch();
-  }
-
-  _dataFetch(base) {
-    return fetch(`http://api.fixer.io/latest?base=${base}`)
-      .then(response => response.json())
-      .then(json => {
-        this.rates = json.rates;
-        this.codes = Object.keys(this.rates);
-        return json;
-      });
-  }
-
-  onSelectChange = e => {
-    const newBase = e.target.value;
-    this._dataFetch(newBase)
-      .then(json => {
-        this.setState({
-          currentBase: newBase
-        });
-      })
-      .catch();
-  };
-
-  render() {
-    return (
-      <div className="App">
+    render() {
+        return (
+            <div className="App">
 
         <ExchangeRatesControls
           codes={this.codes}
@@ -60,10 +92,18 @@ class App extends Component {
           base={this.state.currentBase}
           rates={this.rates}
         />
+        <ExchangeRatesControls
+          codes={this.codes}
+          onChange={this.onSelectChange}
+        />
+        <ExchangeRatesDisplay
+          base={this.state.currentBase}
+          rates={this.historicalRates}
+        />
 
       </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
